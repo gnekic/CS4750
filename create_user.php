42e753729ecd8eb8162788a662cc4424887cca15
Emma Kopf <emmakopf@gmail.com>
2017-04-20 02:07:31 -0400

    create/insert user

---
+++ create_user.php
@@ -0,0 +1,32 @@
+// like an insert?
+<?php
+    require("connect_to_db.php")
+    $db = DbUtil::loginConnection();
+    $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+    
+    if (mysqli_connect_errno()) {
+        echo("Can't connect to MySQL Server. Error code: " .
+             mysqli_connect_error());
+        return null;
+    }
+    
+    $username = $_POST['username']
+    $first_name = $_POST['first_name']
+    $last_name = $_POST['last_name']
+    $email = $_POST['email']
+    $password = $_POST['password']
+    $fav_genre = $_POST['fav_genre']
+    
+    $sql = "INSERT INTO Users Values
+        ('$username', '$first_name', '$last_name', '$email', '$password', '#fav_genre')";
+    
+    if (!mysqli_query($con,$sql))
+    {
+        die('Error: ' . mysqli_error($con));
+    }
+    else
+    {
+        echo "Welcome $username!";
+    }
+    mysqli_close($con);
+?>
