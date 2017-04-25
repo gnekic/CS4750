bd214980237226e7b8d9af7b3413fc98274085f6
Emma Kopf <emmakopf@gmail.com>
2017-04-24 23:39:21 -0400

    new user redirects

--- create_user.php
+++ create_user.php
@@ -1,7 +1,9 @@
 <?php
     ob_start();
     session_start();
-    include_once("./library.php"); // To connect to the database
+   	include("./connect_to_db.php");
+    include_once("./library.php");
+    $db = DbUtil::loginConnection();
     $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
     
     // Check connection
@@ -16,9 +18,8 @@
     $password = md5($_POST['password']); //Encrypt Password
     $fav_genre = $_POST['fav_genre'];
     
-    $_SESSION["username"] = $username;
-    
     //Check if username is unique
+    //FIXME: uncommenting this i can't create any users.
 //    $sql="SELECT username FROM Users";
 //    $result = $db->query($sql);
 //    while($row = $result->fetch_assoc()){
@@ -28,18 +29,23 @@
 //    }
     
     // Form the SQL query (an INSERT query)
-    $sql = "INSERT INTO Users (username, first_name, last_name, email, password, fav_genre) VALUES
-            ('$username', '$first_name', '$last_name', '$email', '$password', '$fav_genre')";
-    
-    if (!mysqli_query($con,$sql)) {
-        die('Error: ' . mysqli_error($con));
+    $stmt = $db->stmt_init();
+    if ($stmt = $db->prepare("INSERT INTO Users (username, first_name, last_name, email, password, fav_genre) VALUES(?, ?, ?, ?, ?, ?)") {
+        $stmt->bind_param('ss', $username, $first_name, $last_name, $email, $password, $fav_genre);
+        $stmt->execute();
+        $stmt->bind_result($name, $pass);
+        if ($stmt->fetch()) {
+            $_SESSION["login"] = true;
+            $_SESSION["username"] = $name;
+            header('Location: ./profile.html');
+            exit();
+        }
+        else{
+            //$_SESSION["wrong"] = 1;
+            header('Location: ./login.html');
+            exit();
+        }
     }
-    else {
-        $_SESSION["login"] = true;
-        $_SESSION["username"] = $username;
-        header('Location : profile.html');
-        exit();
-    }
-    
+        
     mysqli_close($con);
-    ?>
+?>
