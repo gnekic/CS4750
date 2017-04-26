348454b76b66ad5304c1f29f3da04983cdaf12c9
Emma Kopf <emmakopf@gmail.com>
2017-04-25 20:28:44 -0400

    practice.php has right sql and html for popular now

--- practice.php
+++ practice.php
@@ -15,14 +15,18 @@
             $sql="SELECT Title from Movies WHERE Genre = 'Comedy'";
             $result = mysqli_query($con, $sql);
             
-            while($titles = mysqli_fetch_array($result)) {
-                $title = $titles['Title'];
+            while($titles = mysqli_fetch_array($result)){
+                $movies[] = $titles;
+            }
+            
+            foreach($movies as $titles){
+                $title = $titles['Title']; ?>
+                <a href="index.html">
+            <img src="./images/<?php echo($title); ?>.jpg" style="width:304px;height:228px;"></a>
+                <?php
             }
         ?>
         </h1>
-    
-        <a href="index.html">
-<img src="./images/<?php echo($title); ?>.jpg" style="width:304px;height:228px;"></a>
 
     </body>
 </html>
