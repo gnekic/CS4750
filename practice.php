b30c7d31b4336932ff5c1466c1a0ebaee82a3f21
Emma Kopf <emmakopf@gmail.com>
2017-04-25 20:15:32 -0400

    added sql for pictures

---
+++ practice.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <body>
+        <h1>
+        <?php
+            include_once("./library.php");
+            $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+            
+            if (mysqli_connect_errno()) {
+                echo("Can't connect to MySQL Server.");
+                return null;
+            }
+        
+            $sql="SELECT Title from Movies WHERE Genre = 'Comedy'";
+            $result = mysqli_query($con, $sql);
+            
+            while($titles = mysqli_fetch_array($result)) {
+                $title = $titles['Title'];
+            }
+        ?>
+        </h1>
+    
+        <a href="index.html">
+<img src="./images/<?php echo($title); ?>.jpg" style="width:304px;height:228px;"></a>
+
+    </body>
+</html>
